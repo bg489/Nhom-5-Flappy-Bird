a34009369ab579afc59ed03b69de1a34df221f86
leductuanvt1 <leductuan3404@gmail.com>
2025-07-24 23:42:55 +0700

    add more function for enablePipes and disablePipes

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,9 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 {
     public InputAction spawnAction; // Action để bật/tắt spawning
+
+    public GameObject disableThePipes;
+    public GameObject enableThePipes;
+    public GameObject disableThePipesButton;
+
+    [SerializeField] private AudioClip enable;
+    [SerializeField] private AudioClip disable;
+
+    private Image buttonImage;
+
+    private AudioSource audioSource; // Reference to the AudioSource component
 
     public GameObject prefab;
     public float spawnRate = 1f;
@@ -17,6 +31,8 @@ public class Spawner : MonoBehaviour
     {
         spawnAction.Enable();
         spawnAction.performed += OnSpawnActionPerformed;
+        audioSource = GetComponent<AudioSource>(); // Get the AudioSource component attached to this GameObject
+        buttonImage = disableThePipesButton.GetComponent<Image>();
     }
 
     private void OnDisable()
@@ -25,9 +41,36 @@ public class Spawner : MonoBehaviour
         spawnAction.Disable();
     }
 
+    private IEnumerator TemporarilyDisablenablePipes(float duration)
+    {
+        buttonImage.color = Color.red; // Change button color to red when pipes are disabled
+        audioSource.PlayOneShot(disable);
+        disableThePipes.SetActive(true); // bật
+        yield return new WaitForSeconds(duration); // đợi x giây
+        disableThePipes.SetActive(false); // tắt
+    }
+
+    private IEnumerator TemporarilyEnablePipes(float duration)
+    {
+        buttonImage.color = Color.white; // Change button color to green when pipes are enabled
+        audioSource.PlayOneShot(enable);
+        enableThePipes.SetActive(true); // bật
+        yield return new WaitForSeconds(duration); // đợi x giây
+        enableThePipes.SetActive(false); // tắt
+    }
+
+
     private void OnSpawnActionPerformed(InputAction.CallbackContext context)
     {
         // Toggle trạng thái bật/tắt spawn khi nhấn phím
+        if (isSpawned)
+        {
+            StartCoroutine(TemporarilyDisablenablePipes(2f)); // bật trong 2 giây
+        } else
+        {
+            StartCoroutine(TemporarilyEnablePipes(2f)); // bật trong 2 giây
+        }
+
         isSpawned = !isSpawned;
     }
 
